d57a80da00112e38143739588b3e89231e065697
Monesh_Venkul <mvvenkul@gmail.com>
2023-06-27 12:05:41 -0400

    Add files via upload

---
+++ new.js
@@ -0,0 +1,44 @@
+//let student = [97,98,"monesh",96] //array
+
+// let student = {
+//     physics: 97,
+//     maths:98,
+//     chemistry:96,
+//     name:monesh
+// } //object
+
+
+
+//project -> 80% plannaing and algorithms
+
+//           20% is actual coding
+
+
+// function getFirstElement(array) {
+//     return array[0];
+// }
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(getFirstElement(arr)); // 1
+
+
+
+// function findElement(array, element) {
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i] === element) {
+//         return i;
+//       } }
+//     return "not found";
+//   }
+  
+//   let arr = [1, 2, 3, 4, 5];
+//   console.log(findElement(arr, 3)); // 2
+
+// 5,6,8,2,4
+// 5,6,8,2,4,9,5,7,5,2,1,4,8,6,3,5,9,5,7,4,8,6,32,2,1,4,78,9,6,5,4,1,2,6,9,8,5,2,3,6,86
+
+
+
+
+
+ 
